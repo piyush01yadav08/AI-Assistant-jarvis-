8697d16c76e6a1dc2a640cba3bdc564974857c26
Piyush Yadav <2432354.piyush@gmail.com>
2026-04-07 20:19:52 +0530

    Delete Main.py

--- Main.py
+++
@@ -1,8 +0,0 @@
-import cohere
-from dotenv import dotenv_values
-
-env_vars = dotenv_values(".env")
-co = cohere.Client(api_key=env_vars.get("CohereAPIKey"))
-
-models = co.models.list()
-print(models)
